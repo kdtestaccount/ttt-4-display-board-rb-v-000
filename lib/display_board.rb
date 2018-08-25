a9aeec21e109240205fd99cd3f3b674e084f8a86
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-08-25 22:07:33 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-turns = [" ", " ", " ", " ", " ", " ", " ", " ", " ", ]
+turns = [" ", " ", " ", "X", " ", " ", " ", " ", " ", ]
 board = " #{turns[0]} | #{turns[1]} | #{turns[2]} ", "-----------", " #{turns[3]} | #{turns[4]} | #{turns[5]} ", "-----------"," #{turns[6]} | #{turns[7]} | #{turns[8]} "
 puts board
