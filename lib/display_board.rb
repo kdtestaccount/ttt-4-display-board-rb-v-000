2c6383f579ce219bc72badc7a6be7582a5da469c
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-08-25 22:06:57 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,5 @@
 # out the current state.
 
 turns = [" ", " ", " ", " ", " ", " ", " ", " ", " ", ]
-board = " #{turns[0]}  | #{turns[1]}  | #{turns[2]} ", "-----------", " #{turns[3]} | #{turns[4]} | #{turns[5]} ", "-----------","#{turns[6]} | #{turns[7]} | #{turns[8]} "
+board = " #{turns[0]} | #{turns[1]}  | #{turns[2]} ", "-----------", " #{turns[3]} | #{turns[4]} | #{turns[5]} ", "-----------"," #{turns[6]} | #{turns[7]} | #{turns[8]} "
 puts board
