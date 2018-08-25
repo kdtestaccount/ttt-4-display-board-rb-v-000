20c3ce920690e01a517f9eda36e41049b054e658
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-08-25 22:16:04 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,5 +3,5 @@
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", ]
 def display_board(board)
-  print " #{board[0]} | #{board[1]} | #{board[2]} ", "-----------", " #{board[3]} | #{board[4]} | #{board[5]} ", "-----------"," #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} ", "-----------", " #{board[3]} | #{board[4]} | #{board[5]} ", "-----------"," #{board[6]} | #{board[7]} | #{board[8]} "
 end
